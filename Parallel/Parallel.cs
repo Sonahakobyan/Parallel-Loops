25972bd96ffaca2b300a1ec98037435ab6cc2db2
Hakobyan Sona <hakobyansona.98@gmail.com>
2018-04-14 10:14:18 -0700

    Add project files.

---
+++ Parallel/Parallel.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Parallel
+{
+    public static class Parallel
+    {
+        /// <summary>
+        /// Executes a for loop in which iterations may run in parallel.
+        /// </summary>
+        /// <param name="fromInclusive">The start index</param>
+        /// <param name="toExclusive">The end index</param>
+        /// <param name="body">The delegate that is invoked once per iteration</param>
+        public static void ParallelFor(int fromInclusive, int toExclusive, Action<int> body)
+        {
+            if (body == null)
+            {
+                throw new ArgumentNullException("body");
+            }
+
+            if (toExclusive <= fromInclusive)
+            {
+                return;
+            }
+
+            var locker = new object();
+
+            for (int i = fromInclusive; i < toExclusive; i++)
+            {
+                int local = i;
+                Task.Run(() => body(local));
+            }
+        }
+
+
+        /// <summary>
+        /// Executes a foreach operation in which iterations may run in parallel.
+        /// </summary>
+        /// <typeparam name="TSource">Generic type</typeparam>
+        /// <param name="source">An enumerable data source</param>
+        /// <param name="body">The delegate that is invoked once per iteration</param>
+        public static void ParallelForEach<TSource>(IEnumerable<TSource> source, Action<TSource> body)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (body == null)
+            {
+                throw new ArgumentNullException("body");
+            }
+            
+            foreach (var item in source.ToList())
+            {
+                var local = (TSource)item;
+
+                Task.Run(() => body(local));
+            }
+        }
+
+
+        /// <summary>
+        /// Executes a foreach operation in which iterations may run in parallel.
+        /// </summary>
+        /// <typeparam name="TSource"> Generic type</typeparam>
+        /// <param name="source">An enumerable data source</param>
+        /// <param name="parallelOptions">Instance that configures the behavior of this operation</param>
+        /// <param name="body">The delegate that is invoked once per iteration</param>
+        public static void ParallelForEachWithOptions<TSource>(IEnumerable<TSource> source, ParallelOptions parallelOptions, Action<TSource> body)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (parallelOptions == null)
+            {
+                throw new ArgumentNullException("parallelOptions");
+            }
+
+            if (body == null)
+            {
+                throw new ArgumentNullException("body");
+            }
+
+            var loker = new Object();
+            int maxCount = parallelOptions.MaxDegreeOfParallelism;
+            int curCount = 0;
+           
+            int free = 0;
+
+            var locker1 = new Object();
+            var locker2 = new Object();
+            var tasks = new Task[maxCount];
+
+            foreach (var item in source.ToList())
+            {
+                var local = item;
+
+                lock (locker1)
+                {
+                    if (curCount < maxCount)
+                    {
+                        tasks[free] = new Task(() => body(local));
+                        tasks[free].Start();
+                        free++;
+                        curCount++;
+                    }
+                }
+
+
+                if (curCount >= maxCount)
+                {
+                    lock (locker2)
+                    {
+                        free = Task.WaitAny(tasks);
+                        curCount--;
+                    }
+
+                }
+            }
+            Task.WaitAll();
+
+        }
+    }
+}
---
+++ Parallel/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallel
+{
+    class Program
+    {
+        public static void print(int x)
+        {
+            //int n = 1000;
+            //while (--n > 0) ;
+            Console.WriteLine(x);
+        }
+        static void Main(string[] args)
+        {
+            var list = new List<int>();
+            ParallelOptions p = new ParallelOptions();
+            p.MaxDegreeOfParallelism = 16;
+
+            Parallel.ParallelFor(0, 15000, x => list.Add(x));
+            Parallel.ParallelForEachWithOptions(list, p, print);
+
+            //Parallel.ParallelForEach(list, x => print(x));
+
+            //var list = new List<int>();
+            //System.Threading.Tasks.Parallel.For(0, 12350, x => list.Add(x));
+           
+           // System.Threading.Tasks.Parallel.ForEach(list, p, x => Console.WriteLine(x));
+            Console.Read();
+        }
+    }
+}
